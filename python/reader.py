ea12d7a0591f4963b9d37dff5d9e842bc77605a6
whoami <whoami@whoami.com>
2019-06-20 15:21:37 +0800

    update

--- python/reader.py
+++ python/reader.py
@@ -26,7 +26,7 @@ def file_reader(file_dir,
             for filename in files:
                 if not filename.startswith(filename_feature):
                     continue
-                n_line = sum(1 for 1 in open(os.path.join(root, filename), "rb"))
+                n_line = sum(1 for _ in open(os.path.join(root, filename), "rb"))
                 for line in tqdm.tqdm(io.open(os.path.join(root, filename), 'r', encoding='utf8'), desc="tokenization@lac@{} ".format(filename), total=n_line):
                     index += 1
                     bad_line = False
